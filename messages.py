94d369cf6182f60af5633aeaf9d7152438ad9e97
ivanstatkevich <statkevich.ivan999@gmail.com>
2023-05-06 19:21:23 +0300

    added prepared messages and first handler

---
+++ messages.py
@@ -0,0 +1,3 @@
+prepared_messages = {
+    "start_message": 'Write username and IP of your mikrotik.\nFor example "admin 192.168.88.1"'
+}
--- telegram_bot.py
+++ telegram_bot.py
@@ -1,10 +1,18 @@
 import os
-from dotenv import load_dotenv
+from dotenv import load_dotenv, find_dotenv
 import telebot
+from telebot import types
+from messages import prepared_messages
 
-load_dotenv()
+load_dotenv(find_dotenv())
 
-bot_token = os.getenv("VARIABLE_NAME")
+bot_token = os.getenv("BOT_TOKEN")
 bot = telebot.TeleBot(bot_token)
+
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.send_message(message.chat.id, prepared_messages["start_message"])
+
 
 bot.polling(none_stop=True, interval=0)
